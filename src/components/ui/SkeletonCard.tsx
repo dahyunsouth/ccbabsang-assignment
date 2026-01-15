5f8ec0570804d7a6f80778fd37475b90b0af05cb
dahyunsouth <dhnn1536@naver.com>
2026-01-15 23:18:29 +0900

    design: 상품 목록 로딩 UI 개선
      - SkeletonCard의 레이아웃을 ProductCard의 레이아웃과 통일
      - SkeletonCard 개수: '8개'에서 '16개'로 증가

--- src/components/ui/SkeletonCard.tsx
+++ src/components/ui/SkeletonCard.tsx
@@ -8,18 +8,20 @@ export function SkeletonCard() {
       {/* 모바일: 가로형, PC: 세로형 */}
       <div className="flex gap-4 md:flex-col md:gap-0 md:border md:rounded-lg md:overflow-hidden">
         {/* 이미지 영역 */}
-        <div className="w-24 h-24 md:w-full md:aspect-square bg-gray-200 rounded-lg md:rounded-none" />
+        <div className="w-24 h-24 md:w-full md:h-36 bg-gray-200 rounded-lg md:rounded-none" />
 
         {/* 정보 영역 */}
-        <div className="flex-1 md:p-4 space-y-3">
-          {/* 상품명 */}
-          <div className="h-4 bg-gray-200 rounded w-3/4" />
+        <div className="flex-1 md:p-4 flex flex-col justify-between">
+          {/* 재고 뱃지 영역 (ProductCard와 동일) */}
+          <div className="mb-2">
+            <div className="inline-block px-2 py-0.5 w-24 h-5 bg-gray-200 rounded" />
+          </div>
 
-          {/* 가격 */}
-          <div className="h-6 bg-gray-200 rounded w-1/2" />
+          {/* 상품명 (text-sm md:text-base 높이와 유사) */}
+          <div className="h-5 md:h-6 bg-gray-200 rounded w-3/4" />
 
-          {/* 재고 정보 */}
-          <div className="h-3 bg-gray-200 rounded w-2/3" />
+          {/* 가격 (text-lg md:text-xl 높이와 유사) */}
+          <div className="h-7 md:h-8 bg-gray-200 rounded w-1/2 mt-1" />
         </div>
       </div>
     </article>
